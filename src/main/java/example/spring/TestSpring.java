4fa6a253938b988fa9c90e303c0c1baee26c72e5
e-number <eggnom@yandex.ru>
2021-08-31 15:24:29 +0300

    SpringScopePrototype

--- src/main/java/example/spring/TestSpring.java
+++ src/main/java/example/spring/TestSpring.java
@@ -8,12 +8,28 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList();
-
+//
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusicList();
+//
 //        System.out.printf(musicPlayer.getName());
 
         context.close();
